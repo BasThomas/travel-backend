08d17c457d23aec7295d94a0946ca2494c345aa8
CasperSchobers <casperschobers@live.nl>
2016-09-16 14:32:39 +0200

    gray border

--- TravelGuide/resources/views/places.blade.php
+++ TravelGuide/resources/views/places.blade.php
@@ -4,10 +4,11 @@
     <!-- Main component for a primary marketing message or call to action -->
     <h1>Places</h1>
     @foreach($places as $place)
+        <hr style="border-color: #999797;" />
         <h2>{{$place->name}}</h2>
         <div class="row">
             <div class="col-md-4">
-                <div class="embed-responsive embed-responsive-4by3">
+                <div class="embed-responsive embed-responsive-4by3" style="border: 1px solid #999797;">
                     <iframe
                             class="embed-responsive-item"
                             frameborder="0" style="border:0"
@@ -19,12 +20,18 @@
                 @if($place->images())
                     <div class="row grid">
                         @foreach($place->images()->get() as $img)
-                            <div class="col-md-4 grid-item"><a class="fancybox" rel="{{$place->id}}" href="{{URL::to($img->url)}}"><img class="grayscale" src="{{URL::to($img->url)}}" width="100%"/></a></div>
+                            <div class="col-md-4 grid-item">
+                                <a class="fancybox" rel="{{$place->id}}" href="{{URL::to($img->url)}}">
+                                    <img class="grayscale" src="{{URL::to($img->url)}}" width="100%"/>
+                                </a>
+                            </div>
                         @endforeach
                     </div>
                 @endif
             </div>
         </div>
-        <hr />
+        @if ($loop->last)
+            <hr style="border-color: #999797;" />
+        @endif
     @endforeach
 @endsection
